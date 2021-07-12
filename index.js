f4ca04496bb50b1068463bac91410886c2035dfe
Khoa Nguyễn <42999055+khoa19102005@users.noreply.github.com>
2021-07-12 10:00:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,26 +38,4 @@ client.jointocreatemap = new Enmap({
 require(`./modules/cmds`)(client);
 require(`./modules/jointocreate`)(client);
 //login to the BOT
-const prefix = "!";
-
-client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
-
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-  }
-
-  else if (command === "sum") {
-    const numArgs = args.map(x => parseFloat(x));
-    const sum = numArgs.reduce((counter, x) => counter += x);
-    message.reply(`The sum of all the arguments you provided is ${sum}!`);
-  }
-});
-
 client.login(token);
