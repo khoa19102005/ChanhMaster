4cd11bd7d931bda1bce093ae4e6c9f5dc15d2bca
Khoa Nguyễn <42999055+khoa19102005@users.noreply.github.com>
2021-07-12 18:16:54 +0700

    Update cmds.js

--- modules/cmds.js
+++ modules/cmds.js
@@ -12,7 +12,7 @@ module.exports = client => {
    */
   client.category = {
     "general": ["ping", "support", "server", "tutorial", "video", "info", "stats", "stat", "uptime", "add", "invite", "source", "github"],
-    "voice": ["lock", "unlock", "kick", "vcinvite", "vcadd", "voiceinvite", "voiceadd", "ban", "unban", "trust", "untrust", "limit", "bitrate", "promote", "rename"],
+    "voice": ["lock", "unlock", "kick", "vcinvite", "vcadd", "voiceinvite", "voiceadd", "ban", "unban", "trust", "untrust", "limit", "bitrate", "promote", "rename", "transfer"],
     "setup": ["setup", "setupname", "setup2", "setup2name", "setup3", "setup3name", "fix"]
   }
   /** ////////////////////////////////////////// *
